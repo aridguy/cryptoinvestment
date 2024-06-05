058a0fb11f65fbcbac59020c416b31bb3fcfe66c
aridguy <rotimiariyo@gmail.com>
2024-06-05 21:23:30 +0100

    modified and still modifying the invest section of the application

---
+++ investment-datas/investment-data.js
@@ -0,0 +1,37 @@
+var formData = {
+    q1: document.querySelector('input[name="ever_used_crypto_for_payment_before"]:checked').value,
+    q2: document.querySelector('input[name="ever_invested_in_crypto_before"]:checked').value,
+    q3: document.getElementById('q3').value,
+    q4: document.querySelector('input[name="what_cryptocurrency_would_you_purchase"]:checked').value,
+    q5: document.getElementById('q5').value,
+    q6: document.querySelector('input[name="Would_you_like_to_trade_&_invest_in_gold_and_oil"]:checked').value,
+    q7: document.querySelector('input[name="investment_strategy"]:checked').value,
+    q8: document.querySelector('input[name="time_horizon"]:checked').value,
+    q9: document.querySelector('input[name="investment_strategy_appropriate"]:checked').value,
+    q10: document.getElementById('primary_objective').value,
+    q11: document.getElementById('risk_tolerance').value,
+    q12: document.getElementById('definition_of_risk').value,
+    q13: document.querySelector('input[name="greatest_loss_tolerance"]:checked').value,
+    q14: document.getElementById('risk_tolerance_explanation').value,
+    q15: document.querySelector('input[name="investment_decline_response"]:checked').value,
+    q16: document.querySelector('input[name="action_in_2008"]:checked').value,
+    q17: document.getElementById('risk_tolerance_impact_2008').value,
+    q18: document.getElementById('uncomfortable_assets').value,
+    q19: document.getElementById('other_significant_holdings').value,
+    q20: document.getElementById('reasonable_objectives').value,
+    q21: document.getElementById('success_measure_time_period').value,
+    q22: document.getElementById('investment_manager_time').value,
+    q23: document.getElementById('pressing_issues').value,
+    q24: document.getElementById('greatest_concerns').value,
+    q25: document.getElementById('other_issues').value,
+    q26: document.getElementById('spending_liquidity_needs').value,
+    q27: document.getElementById('review_frequency').value,
+    q28: document.getElementById('additional_comments').value
+};
+
+alert("hello")
+Swal.fire({
+    title: "Hello Visitor!",
+    text: "use the contact form below!",
+    icon: "question",
+  });
